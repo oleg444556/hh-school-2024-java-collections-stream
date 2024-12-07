3fb535c81d2abd9c0508c9056ad5fd9f6245da82
The_Pimp <mishanin_oleg@bk.ru>
2024-12-07 16:47:25 +0300

    feat: метод конвертирует список персон в множество строк вида "Имя - регион"

--- src/main/java/tasks/Task6.java
+++ src/main/java/tasks/Task6.java
@@ -2,23 +2,34 @@ package tasks;
 
 import common.Area;
 import common.Person;
+
 import java.util.Collection;
-import java.util.HashSet;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 /*
 Имеются
 - коллекция персон Collection<Person>
 - словарь Map<Integer, Set<Integer>>, сопоставляющий каждой персоне множество id регионов
 - коллекция всех регионов Collection<Area>
-На выходе хочется получить множество строк вида "Имя - регион". Если у персон регионов несколько, таких строк так же будет несколько
+На выходе хочется получить множество строк вида "Имя - регион".
+Если у персон регионов несколько, таких строк так же будет несколько
  */
 public class Task6 {
 
   public static Set<String> getPersonDescriptions(Collection<Person> persons,
                                                   Map<Integer, Set<Integer>> personAreaIds,
                                                   Collection<Area> areas) {
-    return new HashSet<>();
+    Map<Integer, String> areasIdToName = new HashMap<>();
+    for (Area area : areas) {
+      areasIdToName.put(area.getId(), area.getName());
+    }
+    return persons.stream()
+        .flatMap(person -> personAreaIds.get(person.id())
+            .stream()
+            .map(areaId -> person.firstName() + " - " + areasIdToName.get(areaId)))
+        .collect(Collectors.toSet());
   }
 }
